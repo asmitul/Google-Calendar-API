437eedb6ba243bb972616458a0d6db2e4ac941fd
asmitul <asmitullah@gmail.com>
2025-01-01 08:23:23 +0300

    test

--- app/main.py
+++ app/main.py
@@ -83,17 +83,6 @@ class EventCreate(BaseModel):
         except ValueError:
             raise ValueError('Invalid datetime format')
 
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/tasks"]
-
 @app.get("/")
 async def root():
     return {"message": "Welcome to Google Calendar API"}
